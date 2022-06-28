2460884aca83793d39a7345f8540bda9b03e0bd4
Naima Hassan <naima.hassan434@gmail.com>
2022-06-28 20:54:29 +0100

    Cart Container

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import CartContainer from "./components/CartContainer";
 import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <main>
       <NavBar />
+      <CartContainer />
     </main>
   );
 }
