c3a59924f7da8238b57f3d1a30d870ff6c9201ed
Naima Hassan <naima.hassan434@gmail.com>
2022-06-29 13:37:42 +0100

    Reducers - Calculate Totals

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import CartContainer from "./components/CartContainer";
 import NavBar from "./components/NavBar";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { calculateTotals } from "./features/cart/cartSlice";
 
 function App() {
+  const { cartItems } = useSelector((store) => store.cart);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems]);
+
   return (
     <main>
       <NavBar />
